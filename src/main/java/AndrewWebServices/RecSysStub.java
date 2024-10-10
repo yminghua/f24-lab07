2ccbe0cb03d23ce6117934b14c7042b1a6c33c13
yminghua <yminghua8@gmail.com>
2024-10-10 19:35:33 -0400

    finish Stub for RecSys

---
+++ src/main/java/AndrewWebServices/RecSysStub.java
@@ -0,0 +1,8 @@
+package AndrewWebServices;
+
+public class RecSysStub extends RecSys {
+    @Override
+    public String getRecommendation(String accountName) {
+        return "Animal House";
+    }
+}
